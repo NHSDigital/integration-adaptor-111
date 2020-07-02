4aeb7e24a54998c7cc515ec53a29ed806739d60f
Dawid Stelmaszek <d.stelmaszek@kainos.com>
2020-07-02 08:58:39 +0200

    NIAD-345 CarePlan unit tests; corrected LanguageCode position in ITK example request

--- service/src/test/java/uk/nhs/adaptors/oneoneone/cda/report/mapper/CarePlanMapperTest.java
+++ service/src/test/java/uk/nhs/adaptors/oneoneone/cda/report/mapper/CarePlanMapperTest.java
@@ -1,8 +1,14 @@
 package uk.nhs.adaptors.oneoneone.cda.report.mapper;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
 import org.hl7.fhir.dstu3.model.CarePlan;
 import org.hl7.fhir.dstu3.model.Encounter;
-import org.hl7.fhir.dstu3.model.Narrative;
+import org.hl7.fhir.dstu3.model.Patient;
 import org.hl7.fhir.dstu3.model.Period;
 import org.hl7.fhir.dstu3.model.Reference;
 import org.junit.Before;
@@ -13,6 +19,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.w3c.dom.Node;
+
 import uk.nhs.connect.iucds.cda.ucr.CE;
 import uk.nhs.connect.iucds.cda.ucr.CS;
 import uk.nhs.connect.iucds.cda.ucr.POCDMT000002UK01ClinicalDocument1;
@@ -25,19 +32,13 @@ import uk.nhs.connect.iucds.cda.ucr.ST;
 import uk.nhs.connect.iucds.cda.ucr.StrucDocContent;
 import uk.nhs.connect.iucds.cda.ucr.StrucDocText;
 
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
 @RunWith(MockitoJUnitRunner.class)
 public class CarePlanMapperTest {
-    private final String SNOMED = "2.16.840.1.113883.2.1.3.2.4.15";
-    private final String INFORMATION_ADVICE_GIVEN = "1052951000000105";
-
+    private static final String LANG = "EN";
+    private static final String SNOMED = "2.16.840.1.113883.2.1.3.2.4.15";
+    private static final String INFORMATION_ADVICE_GIVEN = "1052951000000105";
     private static final String DESCRIPTION = "description";
-    private static final String TEXT = "text";
+    private static final String DIV_AS_STRING = String.format("<div xmlns=\"http://www.w3.org/1999/xhtml\">%s</div>", DESCRIPTION);
     private static final String TITLE = "title";
 
     @Mock
@@ -65,16 +66,7 @@ public class CarePlanMapperTest {
     private CS cs;
 
     @Mock
-    private Node node;
-
-    @Mock
-    private Narrative narrative;
-
-    @Mock
     private StrucDocText strucDocText;
-
-    @Mock
-    private PeriodMapper periodMapper;
 
     @InjectMocks
     private CarePlanMapper carePlanMapper;
@@ -93,17 +85,17 @@ public class CarePlanMapperTest {
         when(component2.getStructuredBody()).thenReturn(structuredBody);
         when(component3.getSection()).thenReturn(section);
         when(component5.getSection()).thenReturn(section);
-        when(structuredBody.getComponentArray()).thenReturn(new POCDMT000002UK01Component3[]{component3});
+        when(structuredBody.getComponentArray()).thenReturn(new POCDMT000002UK01Component3[] { component3 });
 
         encounter.setPeriod(period);
-        Reference encounterReference = new Reference(encounter);
-
         title.setLanguage("EN");
         cs.setCode("EN");
         mockLanguageXmlStructure(cs);
 
-        when(encounter.getSubject()).thenReturn(encounterReference);
-        when(encounter.getSubjectTarget()).thenReturn(encounter);
+        Patient subject = mock(Patient.class);
+        Reference subjectReference = new Reference(subject);
+        when(encounter.getSubject()).thenReturn(subjectReference);
+        when(encounter.getSubjectTarget()).thenReturn(subject);
         when(encounter.getPeriod()).thenReturn(period);
     }
 
@@ -123,16 +115,16 @@ public class CarePlanMapperTest {
         assertThat(carePlan.getPeriod()).isEqualTo(period);
         assertThat(carePlan.getSubject()).isEqualTo(encounter.getSubject());
         assertThat(carePlan.getSubjectTarget()).isEqualTo(encounter.getSubjectTarget());
-//        assertThat(carePlan.getLanguage()).isEqualTo(cs);
+        assertThat(carePlan.getLanguage()).isEqualTo(LANG);
 
-//        assertThat(carePlan.getText()).isEqualTo(narrative);
+        assertThat(carePlan.getText().getDivAsString()).isEqualTo(DIV_AS_STRING);
         assertThat(carePlan.getTitle()).isEqualTo(TITLE);
         assertThat(carePlan.getDescription()).isEqualTo(DESCRIPTION);
     }
 
     private void mockSection() {
         when(section.sizeOfComponentArray()).thenReturn(2).thenReturn(0);
-        when(section.getComponentArray()).thenReturn(new POCDMT000002UK01Component5[]{component5, component5});
+        when(section.getComponentArray()).thenReturn(new POCDMT000002UK01Component5[] { component5, component5 });
         when(section.getCode()).thenReturn(code);
         when(section.isSetLanguageCode()).thenReturn(true);
         when(section.isSetTitle()).thenReturn(true);
@@ -160,16 +152,18 @@ public class CarePlanMapperTest {
     }
 
     private void mockTitleXmlStructure(ST title) {
+        Node node = mock(Node.class);
         when(title.getDomNode()).thenReturn(node);
-        getNodeValue(TITLE);
+        mockNodeValue(node, TITLE);
     }
 
     private void mockLanguageXmlStructure(CS cs) {
+        Node node = mock(Node.class);
         when(cs.getDomNode()).thenReturn(node);
-        getNodeValue("EN");
+        mockNodeValue(node, LANG);
     }
 
-    private void getNodeValue(String retValue) {
+    private void mockNodeValue(Node node, String retValue) {
         when(node.getFirstChild()).thenReturn(node);
         when(node.getNodeValue()).thenReturn(retValue);
     }
