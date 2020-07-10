2cd6c63744a762c1a2a3e1a701f065addf30798d
Bob King <b.king@kainos.com>
2020-07-10 10:15:16 +0100

    feature/NIAD-396-ITK-Conformance: Initial commit pre-rebase..

--- service/src/integration-test/java/uk/nhs/adaptors/oneoneone/cda/report/controller/ReportControllerIT.java
+++ service/src/integration-test/java/uk/nhs/adaptors/oneoneone/cda/report/controller/ReportControllerIT.java
@@ -27,6 +27,19 @@ import org.springframework.test.context.junit4.SpringRunner;
 import uk.nhs.adaptors.oneoneone.config.AmqpProperties;
 import uk.nhs.adaptors.oneoneone.utils.FhirJsonValidator;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+import java.net.URL;
+import java.nio.file.Paths;
+
+import static io.restassured.RestAssured.given;
+import static java.nio.file.Files.readAllBytes;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.MediaType.APPLICATION_XML_VALUE;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 public class ReportControllerIT {
@@ -48,25 +61,25 @@ public class ReportControllerIT {
     @Test
     public void postReportInvalidBody() {
         given()
-            .port(port)
-            .contentType(APPLICATION_XML_VALUE)
-            .body("<invalid_xml>")
-            .when()
-            .post(REPORT_ENDPOINT)
-            .then()
-            .statusCode(BAD_REQUEST.value()).extract();
+                .port(port)
+                .contentType(APPLICATION_XML_VALUE)
+                .body("<invalid_xml>")
+                .when()
+                .post(REPORT_ENDPOINT)
+                .then()
+                .statusCode(BAD_REQUEST.value()).extract();
     }
 
     @Test
     public void postReportValidBody() throws JMSException {
         given()
-            .port(port)
-            .contentType(APPLICATION_XML_VALUE)
-            .body(getResourceAsString("/xml/ITK_Report_request.xml"))
-            .when()
-            .post(REPORT_ENDPOINT)
-            .then()
-            .statusCode(ACCEPTED.value());
+                .port(port)
+                .contentType(APPLICATION_XML_VALUE)
+                .body(getResourceAsString("/xml/ITK_Report_request.xml"))
+                .when()
+                .post(REPORT_ENDPOINT)
+                .then()
+                .statusCode(OK.value());
 
         Message jmsMessage = jmsTemplate.receive(amqpProperties.getQueueName());
         String messageBody = jmsMessage.getBody(String.class);
--- service/src/main/java/uk/nhs/adaptors/oneoneone/cda/report/controller/ReportController.java
+++ service/src/main/java/uk/nhs/adaptors/oneoneone/cda/report/controller/ReportController.java
@@ -12,8 +12,35 @@ import static uk.nhs.adaptors.oneoneone.xml.XmlValidator.validate;
 
 import java.util.Map;
 
+import static org.hl7.fhir.dstu3.model.IdType.newRandomUuid;
+import static org.springframework.http.HttpStatus.ACCEPTED;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.MediaType.APPLICATION_XML_VALUE;
+import static org.springframework.http.MediaType.TEXT_XML_VALUE;
+
+import static uk.nhs.adaptors.oneoneone.cda.report.controller.utils.ReportRequestUtils.extractClinicalDocument;
+import static uk.nhs.adaptors.oneoneone.xml.XmlValidator.validate;
+
+import java.util.Collections;
+import java.util.Map;
+
+import javax.xml.soap.MessageFactory;
+import javax.xml.soap.Name;
+import javax.xml.soap.SOAPBody;
+import javax.xml.soap.SOAPBodyElement;
+import javax.xml.soap.SOAPElement;
+import javax.xml.soap.SOAPEnvelope;
+import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPHeader;
+import javax.xml.soap.SOAPMessage;
+import javax.xml.soap.SOAPPart;
+
 import org.apache.xmlbeans.XmlException;
 import org.dom4j.DocumentException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -31,26 +58,58 @@ import uk.nhs.connect.iucds.cda.ucr.POCDMT000002UK01ClinicalDocument1;
 @AllArgsConstructor
 @Slf4j
 public class ReportController {
+    public static final String ACTION_RESPONSE = "urn:nhs-itk:services:201005:SendNHS111Report-v2-0Response";
+    public static final String HTTP_HEADER_MESSAGE_ID = "MessageId";
 
     private final EncounterReportService encounterReportService;
 
-    @PostMapping(value = "/report", consumes = {TEXT_XML_VALUE, APPLICATION_XML_VALUE})
+    @PostMapping(value = "/report",
+        consumes = {TEXT_XML_VALUE, APPLICATION_XML_VALUE},
+        produces = {TEXT_XML_VALUE, APPLICATION_XML_VALUE})
     @ResponseStatus(value = ACCEPTED)
-    public void postReport(@RequestBody String reportXml) {
+    public ResponseEntity<?> postReport(@RequestBody String reportXml) {
         try {
             Map<ReportElement, String> reportElementsMap = ReportParserUtil.parseReportXml(reportXml);
             LOGGER.info("ITK SOAP message received. MessageId: {}, ItkTrackingId: {}",
                 reportElementsMap.get(MESSAGE_ID), reportElementsMap.get(TRACKING_ID));
-
             POCDMT000002UK01ClinicalDocument1 clinicalDocument = extractClinicalDocument(reportElementsMap
                 .get(ReportElement.DISTRIBUTION_ENVELOPE));
             validate(clinicalDocument);
 
-            encounterReportService.transformAndPopulateToGP(clinicalDocument, reportElementsMap.get(MESSAGE_ID),
-                reportElementsMap.get(TRACKING_ID));
-        } catch (XmlException | DocumentException e) {
+            encounterReportService.transformAndPopulateToGP(clinicalDocument,
+                reportElementsMap.get(ReportElement.MESSAGE_ID));
+
+            MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+            String messageID = reportElementsMap.get(ReportElement.MESSAGE_ID);
+            headers.put(HTTP_HEADER_MESSAGE_ID, Collections.singletonList(messageID));
+
+            SOAPMessage message = createSimpleMessageResponseSOAPPart(messageID);
+
+            return new ResponseEntity<>(message, headers, HttpStatus.OK);
+        } catch (XmlException | DocumentException | SOAPException e) {
             LOGGER.error(BAD_REQUEST.toString() + e.getMessage());
             throw new ResponseStatusException(BAD_REQUEST, e.getMessage());
         }
+    }
+
+    private SOAPMessage createSimpleMessageResponseSOAPPart(String messageID) throws SOAPException {
+        SOAPMessage message = MessageFactory.newInstance().createMessage();
+        SOAPPart sPart = message.getSOAPPart();
+        SOAPEnvelope env = sPart.getEnvelope();
+
+        SOAPHeader header = env.getHeader();
+        header.addHeaderElement(env.createName("MessageID", "wsa", ""))
+            .addTextNode(String.valueOf(newRandomUuid()));
+        header.addHeaderElement(env.createName("Action", "wsa", ""))
+            .addTextNode(ACTION_RESPONSE);
+
+        SOAPBody body = env.getBody();
+        Name ns = env.createName("SimpleMessageResponse", "itk:",
+            "urn:nhs-itk:ns:201005");
+        SOAPBodyElement bodyElement = body.addBodyElement(ns);
+        SOAPElement ele = bodyElement.addChildElement(ns);
+        ele.addTextNode("MESSAGE_ID:" + messageID);
+
+        return message;
     }
 }
