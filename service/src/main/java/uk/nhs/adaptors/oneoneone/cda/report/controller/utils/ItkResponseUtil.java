74398dccf4d01caf632bdad7db73fbbf99535550
Dawid Stelmaszek <d.stelmaszek@kainos.com>
2020-07-14 15:29:05 +0200

    NIAD-396 using ClassPathResource class to load resources

--- service/src/main/java/uk/nhs/adaptors/oneoneone/cda/report/controller/utils/ItkResponseUtil.java
+++ service/src/main/java/uk/nhs/adaptors/oneoneone/cda/report/controller/utils/ItkResponseUtil.java
@@ -1,17 +1,18 @@
 package uk.nhs.adaptors.oneoneone.cda.report.controller.utils;
 
-import static java.nio.file.Files.readAllBytes;
+import static java.nio.charset.StandardCharsets.UTF_8;
 
-import java.net.URL;
-import java.nio.file.Paths;
+import java.io.IOException;
 
+import org.apache.commons.io.IOUtils;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ItkResponseUtil {
 
     private static final String SUCCESS_TEMPLATE = loadSuccessTemplate();
-    private static final String SOAP_ITK_OK_RESPONSE_TEMPLATE_PATH = "/itk/SoapItkOkResponseTemplate.xml";
+    private static final String SOAP_ITK_OK_RESPONSE_TEMPLATE_PATH = "itk/SoapItkOkResponseTemplate.xml";
 
     public String createSuccessResponseEntity(String originalMessageId, String responseMessageId) {
         return String.format(SUCCESS_TEMPLATE, responseMessageId, originalMessageId);
@@ -23,9 +24,8 @@ public class ItkResponseUtil {
 
     private static String getResourceAsString(String path) {
         try {
-            URL reportXmlResource = ItkResponseUtil.class.getResource(path);
-            return new String(readAllBytes(Paths.get(reportXmlResource.getPath())));
-        } catch (Exception e) {
+            return IOUtils.toString(new ClassPathResource(path).getInputStream(), UTF_8);
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
