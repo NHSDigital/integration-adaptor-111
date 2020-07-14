f937d4ffe924b8d1bac7f2626875b351736a5161
Broom94 <42891767+Broom94@users.noreply.github.com>
2020-07-14 08:17:42 +0100

    Removed duplicated participant from previous pr, added effective time to related entity in ITK report, created Relatedperson mapper plus unit tests, added relatedpersonmapper to participantMapper, add this new praticipantmapper method to encounter to add related person. Related person now exists in bundle. Reformatted code and file.

--- service/src/main/java/uk/nhs/adaptors/oneoneone/cda/report/mapper/InformantMapper.java
+++ service/src/main/java/uk/nhs/adaptors/oneoneone/cda/report/mapper/InformantMapper.java
@@ -1,22 +1,23 @@
 package uk.nhs.adaptors.oneoneone.cda.report.mapper;
 
-import lombok.AllArgsConstructor;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
 import org.hl7.fhir.dstu3.model.CodeableConcept;
 import org.hl7.fhir.dstu3.model.Encounter;
 import org.hl7.fhir.dstu3.model.Practitioner;
 import org.hl7.fhir.dstu3.model.Reference;
 import org.springframework.stereotype.Component;
-import uk.nhs.connect.iucds.cda.ucr.POCDMT000002UK01Informant12;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import lombok.AllArgsConstructor;
+import uk.nhs.connect.iucds.cda.ucr.POCDMT000002UK01Informant12;
 
 @Component
 @AllArgsConstructor
 public class InformantMapper {
 
-    private PractitionerMapper practitionerMapper;
+    private final PractitionerMapper practitionerMapper;
 
     public Optional<Encounter.EncounterParticipantComponent> mapInformantIntoParticipantComponent(POCDMT000002UK01Informant12 informant) {
         if (informant.isSetAssignedEntity()) {
@@ -31,22 +32,8 @@ public class InformantMapper {
         }
     }
 
-    public Optional<Encounter.EncounterParticipantComponent>
-    mapInformantRelatedPersonIntoParticipantComponent(POCDMT000002UK01Informant12 informant) {
-        if (informant.isSetRelatedEntity()) {
-            Encounter.EncounterParticipantComponent component = new Encounter.EncounterParticipantComponent();
-            component.setType(retrieveTypeFromITK(informant));
-            Practitioner practitioner = practitionerMapper.mapPractitioner(informant.getRelatedEntity());
-            component.setIndividualTarget(practitioner);
-            component.setIndividual(new Reference(practitioner));
-            return Optional.of(component);
-        } else {
-            return Optional.empty();
-        }
-    }
-
     private List<CodeableConcept> retrieveTypeFromITK(POCDMT000002UK01Informant12 informant) {
         return Collections.singletonList(new CodeableConcept()
-                .setText(informant.getTypeCode()));
+            .setText(informant.getTypeCode()));
     }
 }
--- service/src/test/java/uk/nhs/adaptors/oneoneone/cda/report/mapper/PractitionerMapperTest.java
+++ service/src/test/java/uk/nhs/adaptors/oneoneone/cda/report/mapper/PractitionerMapperTest.java
@@ -1,5 +1,8 @@
 package uk.nhs.adaptors.oneoneone.cda.report.mapper;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
 import org.hl7.fhir.dstu3.model.Address;
 import org.hl7.fhir.dstu3.model.ContactPoint;
 import org.hl7.fhir.dstu3.model.HumanName;
@@ -10,17 +13,14 @@ import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+
 import uk.nhs.connect.iucds.cda.ucr.AD;
 import uk.nhs.connect.iucds.cda.ucr.PN;
 import uk.nhs.connect.iucds.cda.ucr.POCDMT000002UK01AssignedAuthor;
 import uk.nhs.connect.iucds.cda.ucr.POCDMT000002UK01AssignedEntity;
 import uk.nhs.connect.iucds.cda.ucr.POCDMT000002UK01AssociatedEntity;
 import uk.nhs.connect.iucds.cda.ucr.POCDMT000002UK01Person;
-import uk.nhs.connect.iucds.cda.ucr.POCDMT000002UK01RelatedEntity;
 import uk.nhs.connect.iucds.cda.ucr.TEL;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PractitionerMapperTest {
@@ -66,6 +66,24 @@ public class PractitionerMapperTest {
         assertThat(practitioner.getAddressFirstRep()).isEqualTo(address);
     }
 
+    private POCDMT000002UK01Person createPerson() {
+        POCDMT000002UK01Person person = POCDMT000002UK01Person.Factory.newInstance();
+        PN pn = PN.Factory.newInstance();
+        PN[] pnArray = {pn};
+        person.setNameArray(pnArray);
+        return person;
+    }
+
+    private TEL[] createTelecomArray() {
+        TEL tel = TEL.Factory.newInstance();
+        return new TEL[] {tel};
+    }
+
+    private AD[] createAddrArray() {
+        AD ad = AD.Factory.newInstance();
+        return new AD[] {ad};
+    }
+
     @Test
     public void shouldMapPractitionerFromAssociatedEntity() {
         POCDMT000002UK01AssociatedEntity associatedEntity = POCDMT000002UK01AssociatedEntity.Factory.newInstance();
@@ -98,49 +116,11 @@ public class PractitionerMapperTest {
         when(addressMapper.mapAddress(ArgumentMatchers.any())).thenReturn(address);
 
         Practitioner practitioner = practitionerMapper.mapPractitioner(associatedEntity);
-
-        assertThat(practitioner.getIdElement().getValue()).startsWith("urn:uuid:");
+        String uuidBeginning = "urn:uuid:";
+        assertThat(practitioner.getIdElement().getValue()).startsWith(uuidBeginning);
         assertThat(practitioner.getActive()).isEqualTo(true);
         assertThat(practitioner.getNameFirstRep()).isEqualTo(humanName);
         assertThat(practitioner.getTelecomFirstRep()).isEqualTo(contactPoint);
         assertThat(practitioner.getAddressFirstRep()).isEqualTo(address);
-    }
-
-    @Test
-    public void shouldMapPractitionerForRelatedEntity() {
-        POCDMT000002UK01RelatedEntity relatedEntity = POCDMT000002UK01RelatedEntity.Factory.newInstance();
-        relatedEntity.setRelatedPerson(createPerson());
-        relatedEntity.setTelecomArray(createTelecomArray());
-        relatedEntity.setAddrArray(createAddrArray());
-
-        when(humanNameMapper.mapHumanName(ArgumentMatchers.any())).thenReturn(humanName);
-        when(contactPointMapper.mapContactPoint(ArgumentMatchers.any())).thenReturn(contactPoint);
-        when(addressMapper.mapAddress(ArgumentMatchers.any())).thenReturn(address);
-
-        Practitioner practitioner = practitionerMapper.mapPractitioner(relatedEntity);
-
-        assertThat(practitioner.getIdElement().getValue()).startsWith("urn:uuid:");
-        assertThat(practitioner.getActive()).isEqualTo(true);
-        assertThat(practitioner.getNameFirstRep()).isEqualTo(humanName);
-        assertThat(practitioner.getTelecomFirstRep()).isEqualTo(contactPoint);
-        assertThat(practitioner.getAddressFirstRep()).isEqualTo(address);
-    }
-
-    private AD[] createAddrArray() {
-        AD ad = AD.Factory.newInstance();
-        return new AD[]{ad};
-    }
-
-    private TEL[] createTelecomArray() {
-        TEL tel = TEL.Factory.newInstance();
-        return new TEL[]{tel};
-    }
-
-    private POCDMT000002UK01Person createPerson() {
-        POCDMT000002UK01Person person = POCDMT000002UK01Person.Factory.newInstance();
-        PN pn = PN.Factory.newInstance();
-        PN[] pnArray = {pn};
-        person.setNameArray(pnArray);
-        return person;
     }
 }
