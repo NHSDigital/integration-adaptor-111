bc78f6bb8597f667163ace82d549a256b2b9e870
Peter van der Merwe <p.vandermerwe@kainos.com>
2020-08-13 11:21:08 +0100

    Changes to test-scripts

--- test-scripts/0.1.0/run.sh
+++ test-scripts/0.1.0/run.sh
@@ -6,10 +6,5 @@ NC='\033[0m'
 echo -e "${LIGHT_GREEN}Stopping running containers${NC}" 
 docker-compose -f docker-compose.yml down
 
-echo -e "${LIGHT_GREEN}Build the docker image for 111${NC}"
-cd ../..
-docker build -t local/111:latest .
-cd test-scripts/0.1.0
-
 echo -e "${LIGHT_GREEN}Build and starting containers${NC}" 
 docker-compose -f docker-compose.yml up -d --no-build
