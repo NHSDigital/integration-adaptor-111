f53348f9390865b40e419fbc1d1343a96efecd59
Peter van der Merwe <p.vandermerwe@kainos.com>
2020-11-18 11:41:34 +0000

    Cleanup

--- 0.2.0/tests/acceptance.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-curl -i --location --request POST 'http://localhost:8080/report' \
-     --data '@../../../service/src/integration-test/resources/xml/ITK_Report_request.xml' \
-     -H "Content-Type: application/xml"
