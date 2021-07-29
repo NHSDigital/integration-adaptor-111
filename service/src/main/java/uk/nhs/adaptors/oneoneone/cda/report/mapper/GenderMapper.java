1cde022f68d1fb19b2e02eeea8db7d047744f852
ssokolowskisebastian <ssokolowskisebastian@gmail.com>
2021-07-29 11:35:40 +0200

    NIAD-1643 changing class name GenderUtil->GenderMapper and cleannig code

---
+++ service/src/main/java/uk/nhs/adaptors/oneoneone/cda/report/mapper/GenderMapper.java
@@ -0,0 +1,15 @@
+package uk.nhs.adaptors.oneoneone.cda.report.mapper;
+
+import static org.hl7.fhir.dstu3.model.Enumerations.*;
+import static org.hl7.fhir.dstu3.model.Enumerations.AdministrativeGender.*;
+
+import java.util.Map;
+
+public class GenderMapper {
+
+    private static final Map<String, AdministrativeGender> mapping = Map.of("1", MALE, "2", FEMALE, "9", OTHER);
+
+    public static AdministrativeGender getGenderFromCode(String genderCode) {
+        return mapping.getOrDefault(genderCode, UNKNOWN);
+    }
+}
