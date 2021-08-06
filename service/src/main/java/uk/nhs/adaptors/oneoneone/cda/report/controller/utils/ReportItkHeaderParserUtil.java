d1bcc3a515cb94141d52abe2e238bc82bc98a5e2
mrogowska <m.rogowska@kainos.com>
2021-08-06 16:53:15 +0200

    NIAD-1670: Add unit tests for ReportItkHeaderParserUtil

--- service/src/main/java/uk/nhs/adaptors/oneoneone/cda/report/controller/utils/ReportItkHeaderParserUtil.java
+++ service/src/main/java/uk/nhs/adaptors/oneoneone/cda/report/controller/utils/ReportItkHeaderParserUtil.java
@@ -13,12 +13,17 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ReportItkHeaderParserUtil {
-    private static final String SPECIFICATION_NODE = "//*[local-name()='spec']";
-    private static final String ITK_ADDRESS_NODE = "//*[local-name()='addresslist']/*[local-name()='address']";
+    public static final String SPECIFICATION_NODE = "//*[local-name()='spec']";
+    public static final String ITK_ADDRESS_NODE = "//*[local-name()='addresslist']/*[local-name()='address']";
+    public static final String TYPE_ATTRIBUTE = "type";
+    public static final String URI_ATTRIBUTE = "uri";
+    public static final String KEY_ATTRIBUTE = "key";
+    public static final String VALUE_ATTRIBUTE = "value";
+    public static final String TRACKING_ID_ATTRIBUTE = "trackingid";
 
     public ItkReportHeader getHeaderValues(Element headerElement) {
         ItkReportHeader header = new ItkReportHeader();
-        header.setTrackingId(headerElement.attributeValue("trackingid"));
+        header.setTrackingId(headerElement.attributeValue(TRACKING_ID_ATTRIBUTE));
         setSpecification(header, headerElement);
         setAddressList(header, headerElement);
 
@@ -28,17 +33,17 @@ public class ReportItkHeaderParserUtil {
     private void setAddressList(ItkReportHeader header, Element headerElement) {
         List<Element> elements = getElements(headerElement, ITK_ADDRESS_NODE);
         Optional<String> dosServiceId = elements.stream()
-            .filter(it -> it.attribute("type") != null
-                && it.attributeValue("type").equals("2.16.840.1.113883.2.1.3.2.4.18.44")
+            .filter(it -> it.attribute(TYPE_ATTRIBUTE) != null
+                && it.attributeValue(TYPE_ATTRIBUTE).equals("2.16.840.1.113883.2.1.3.2.4.18.44")
             )
-            .map(it -> "DOSServiceID:".concat(it.attributeValue("uri")))
+            .map(it -> "DOSServiceID:".concat(it.attributeValue(URI_ATTRIBUTE)))
             .findFirst();
 
         Optional<String> odsCode = elements.stream()
-            .filter(it -> it.attribute("type") == null
-                && it.attributeValue("uri").startsWith("urn:nhs-uk:addressing:ods:")
+            .filter(it -> it.attribute(TYPE_ATTRIBUTE) == null
+                && it.attributeValue(URI_ATTRIBUTE).startsWith("urn:nhs-uk:addressing:ods:")
             )
-            .map(it -> it.attributeValue("uri"))
+            .map(it -> it.attributeValue(URI_ATTRIBUTE))
             .findFirst();
 
         String address = Stream.of(odsCode, dosServiceId)
@@ -51,8 +56,8 @@ public class ReportItkHeaderParserUtil {
 
     private void setSpecification(ItkReportHeader header, Element headerElement) {
         Element spec = getElements(headerElement, SPECIFICATION_NODE).get(0);
-        header.setSpecKey(spec.attributeValue("key"));
-        header.setSpecVal(spec.attributeValue("value"));
+        header.setSpecKey(spec.attributeValue(KEY_ATTRIBUTE));
+        header.setSpecVal(spec.attributeValue(VALUE_ATTRIBUTE));
     }
 
     private List<Element> getElements(Element element, String xpath) {
