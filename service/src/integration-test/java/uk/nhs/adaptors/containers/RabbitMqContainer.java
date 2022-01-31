e1d2fb37116482a39dfdf2ba6716cf07b3825dd4
Alkis Petrou <petroualkis@gmail.com>
2022-01-31 22:47:42 +0000

    RabbitMqContainer touch ups

--- service/src/integration-test/java/uk/nhs/adaptors/containers/RabbitMqContainer.java
+++ service/src/integration-test/java/uk/nhs/adaptors/containers/RabbitMqContainer.java
@@ -4,12 +4,12 @@ import org.testcontainers.containers.GenericContainer;
 
 public final class RabbitMqContainer extends GenericContainer<RabbitMqContainer> {
 
-    public static final int ACTIVEMQ_PORT = 5672;
+    public static final int RABBITMQ_PORT = 5671;
     private static RabbitMqContainer container;
 
     private RabbitMqContainer() {
         super("rabbitmq:latest");
-        addExposedPort(ACTIVEMQ_PORT);
+        addExposedPort(RABBITMQ_PORT);
     }
 
     public static RabbitMqContainer getInstance() {
@@ -25,7 +25,7 @@ public final class RabbitMqContainer extends GenericContainer<RabbitMqContainer>
         System.setProperty("PEM111_AMQP_BROKER", getContainerIpAddress());
         System.setProperty("PEM111_AMQP_USERNAME", "guest");
         System.setProperty("PEM111_AMQP_PASSWORD", "guest");
-        System.setProperty("PEM111_AMQP_PORT", String.valueOf(getMappedPort(ACTIVEMQ_PORT)));
+        System.setProperty("PEM111_AMQP_PORT", String.valueOf(getMappedPort(RABBITMQ_PORT)));
         System.setProperty("PEM111_AMQP_PROTOCOL", "0-9-1");
         System.setProperty("PEM111_AMQP_SSL_ENABLED", "false");
         System.setProperty("PEM111_AMQP_EXCHANGE", "");
