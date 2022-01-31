bcbee9364c59e33d99498aaefae1036fbccbf221
Alkis Petrou <petroualkis@gmail.com>
2022-01-31 14:02:52 +0000

    Conditional ConnectionFactory configuration for AMQP 0.9.1

--- service/src/main/java/uk/nhs/adaptors/oneoneone/config/AmqpConfiguration.java
+++ service/src/main/java/uk/nhs/adaptors/oneoneone/config/AmqpConfiguration.java
@@ -2,11 +2,14 @@ package uk.nhs.adaptors.oneoneone.config;
 
 import static org.springframework.util.StringUtils.isEmpty;
 
+import com.rabbitmq.jms.admin.RMQConnectionFactory;
 import org.apache.qpid.jms.JmsConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
+import javax.jms.ConnectionFactory;
+import java.util.Objects;
 
 @Configuration
 public class AmqpConfiguration {
@@ -17,19 +20,33 @@ public class AmqpConfiguration {
     }
 
     @Bean
-    public JmsConnectionFactory jmsConnectionFactory(AmqpProperties properties) {
-        JmsConnectionFactory factory = new JmsConnectionFactory();
+    public ConnectionFactory jmsConnectionFactory(AmqpProperties properties) {
 
-        factory.setRemoteURI(properties.getBroker());
+        if(Objects.equals(properties.getProtocol(), "0-9-1")) {
+            RMQConnectionFactory connectionFactory = new RMQConnectionFactory();
+            connectionFactory.setUsername(properties.getUsername());
+            connectionFactory.setPassword(properties.getPassword());
+            connectionFactory.setVirtualHost("/");
+            connectionFactory.setHost(properties.getBroker());
+            connectionFactory.setPort(properties.getPort());
+            connectionFactory.setSsl(properties.isSslEnabled());
 
-        if (!isEmpty(properties.getUsername())) {
-            factory.setUsername(properties.getUsername());
+            return connectionFactory;
+
+        } else {
+            JmsConnectionFactory factory = new JmsConnectionFactory();
+
+            factory.setRemoteURI(properties.getBroker());
+
+            if (!isEmpty(properties.getUsername())) {
+                factory.setUsername(properties.getUsername());
+            }
+
+            if (!isEmpty(properties.getPassword())) {
+                factory.setPassword(properties.getPassword());
+            }
+
+            return factory;
         }
-
-        if (!isEmpty(properties.getPassword())) {
-            factory.setPassword(properties.getPassword());
-        }
-
-        return factory;
     }
 }
