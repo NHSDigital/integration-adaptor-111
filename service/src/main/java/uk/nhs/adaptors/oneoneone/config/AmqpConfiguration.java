c1b7ee4fa7ef636de4a8d9a99a6f9e1b9943e44d
Alkis Petrou <petroualkis@gmail.com>
2022-01-31 22:48:46 +0000

    Conditional AmqpConfiguration based on protocol

--- service/src/main/java/uk/nhs/adaptors/oneoneone/config/AmqpConfiguration.java
+++ service/src/main/java/uk/nhs/adaptors/oneoneone/config/AmqpConfiguration.java
@@ -3,12 +3,15 @@ package uk.nhs.adaptors.oneoneone.config;
 import static org.springframework.util.StringUtils.isEmpty;
 
 import com.rabbitmq.jms.admin.RMQConnectionFactory;
+import com.rabbitmq.jms.admin.RMQDestination;
 import org.apache.qpid.jms.JmsConnectionFactory;
+import org.apache.qpid.jms.JmsQueue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
 import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
 import java.util.Objects;
 
 @Configuration
@@ -20,9 +23,26 @@ public class AmqpConfiguration {
     }
 
     @Bean
+    public Destination jmsDestination(AmqpProperties properties) {
+
+        if(Objects.equals(properties.getProtocol(), "0-9-1")) {
+
+            RMQDestination jmsDestination = new RMQDestination();
+            jmsDestination.setAmqpExchangeName(properties.getExchange());
+            jmsDestination.setAmqp(true);
+            jmsDestination.setAmqpRoutingKey(properties.getQueueName());
+
+            return jmsDestination;
+        } else {
+            return new JmsQueue(properties.getQueueName());
+        }
+    }
+
+    @Bean
     public ConnectionFactory jmsConnectionFactory(AmqpProperties properties) {
 
         if(Objects.equals(properties.getProtocol(), "0-9-1")) {
+
             RMQConnectionFactory connectionFactory = new RMQConnectionFactory();
             connectionFactory.setUsername(properties.getUsername());
             connectionFactory.setPassword(properties.getPassword());
@@ -32,8 +52,8 @@ public class AmqpConfiguration {
             connectionFactory.setSsl(properties.isSslEnabled());
 
             return connectionFactory;
-
         } else {
+
             JmsConnectionFactory factory = new JmsConnectionFactory();
 
             factory.setRemoteURI(properties.getBroker());
@@ -49,4 +69,6 @@ public class AmqpConfiguration {
             return factory;
         }
     }
+
+
 }
