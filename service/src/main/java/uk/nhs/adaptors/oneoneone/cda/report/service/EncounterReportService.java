4f20c888abe8835647fd86332a825cde4b20c3c5
Alkis Petrou <petroualkis@gmail.com>
2022-01-31 22:49:08 +0000

    Use Destination configuration from AmqpConfiguration for JmsTemplate

--- service/src/main/java/uk/nhs/adaptors/oneoneone/cda/report/service/EncounterReportService.java
+++ service/src/main/java/uk/nhs/adaptors/oneoneone/cda/report/service/EncounterReportService.java
@@ -1,17 +1,15 @@
 package uk.nhs.adaptors.oneoneone.cda.report.service;
 
+import javax.jms.Destination;
 import javax.jms.TextMessage;
-
 import org.apache.xmlbeans.XmlException;
 import org.hl7.fhir.dstu3.model.Bundle;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
-
 import ca.uhn.fhir.context.FhirContext;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import uk.nhs.adaptors.oneoneone.cda.report.controller.utils.ItkReportHeader;
-import uk.nhs.adaptors.oneoneone.config.AmqpProperties;
 import uk.nhs.connect.iucds.cda.ucr.POCDMT000002UK01ClinicalDocument1;
 
 @Service
@@ -25,19 +23,20 @@ public class EncounterReportService {
 
     private final JmsTemplate jmsTemplate;
 
-    private final FhirContext fhirContext;
+    private Destination destination;
 
-    private final AmqpProperties amqpProperties;
+    private final FhirContext fhirContext;
 
     public void transformAndPopulateToGP(POCDMT000002UK01ClinicalDocument1 clinicalDocumentDocument,
         String messageId, ItkReportHeader header) throws XmlException {
         Bundle encounterBundle = encounterReportBundleService.createEncounterBundle(clinicalDocumentDocument, header, messageId);
 
-        jmsTemplate.send(amqpProperties.getQueueName(), session -> {
+        jmsTemplate.send(destination, session -> {
             TextMessage message = session.createTextMessage(toJsonString(encounterBundle));
             message.setStringProperty(MESSAGE_ID, messageId);
             return message;
         });
+
         LOGGER.info("Successfully sent FHIR message to queue. MessageId: {}, ItkTrackingId: {}", messageId, header.getTrackingId());
     }
 
