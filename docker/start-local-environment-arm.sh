ca6fc0d6a8c05ccd46424f44038942a6119b148b
Rio Knightley <rio.knightley@madetech.com>
2022-05-09 16:56:17 +0100

    Add ARM chip scripts

---
+++ docker/start-local-environment-arm.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -x -e
+
+
+if [[ "$(docker network ls | grep "111network")" == "" ]] ; then
+    docker network create 111network
+fi
+docker-compose -f docker-compose-arm.yml build wiremock activemq;
+docker-compose -f docker-compose-arm.yml up -d wiremock activemq;
+docker-compose -f docker-compose-arm.yml build integration-adaptor-111;
+docker-compose -f docker-compose-arm.yml up integration-adaptor-111;
