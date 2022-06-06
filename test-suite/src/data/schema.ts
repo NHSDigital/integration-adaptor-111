365e20293b0c7a41e564279942c1a918c2fc93e7
Rob Tuppen <robert.tuppen@answerdigital.com>
2022-06-06 10:28:29 +0100

    Add primary emergency request and add schema details to populate fields

--- test-suite/src/data/schema.ts
+++ test-suite/src/data/schema.ts
@@ -110,6 +110,108 @@ const schema: Schema = {
             ],
           },
         },
+        {
+          testName: "Patient Referred to Primary Care for Assessment",
+          testDescription:
+            "111 Report indicating that a call has resulted in the patient being referred to GP for assessment.",
+          template: require("./primary-emergency-itk-request.xml"),
+          testSpecifications: {
+            [RequestHeaderProps.Header]: [
+              {
+                label: "Url",
+                id: "url",
+                defaultValue: "http://localhost:8081/report",
+                validators: [isLocalUrl(), isReportUrl()],
+              },
+              {
+                label: "Content Type",
+                id: "content-type",
+                defaultValue: "application/xml",
+                validators: [notNull(), minLength(7)],
+              },
+            ],
+            [RequestHeaderProps.Body]: [
+              {
+                label: "ODS Code",
+                id: "ods-code",
+                defaultValue: "EM396",
+                validators: [notNull()],
+              },
+              {
+                label: "DOS Code",
+                id: "dos-code",
+                defaultValue: "26428",
+                validators: [],
+              },
+              {
+                label: "NHS Number",
+                id: "nhs-number",
+                defaultValue: "1112223344",
+                validators: [notNull(), isNumeric(), isLength(10)],
+              },
+              {
+                label: "Recipient Name",
+                id: "recipent-name",
+                defaultValue: "John Stones",
+                validators: [notNull(), isAlpha()],
+              },
+              {
+                label: "Recipient Address Line 1",
+                id: "recipent-street-address-line-1",
+                defaultValue: "99 Made up Address",
+                validators: [notNull()],
+              },
+              {
+                label: "Recipient Address Line 2",
+                id: "recipent-street-address-line-2",
+                defaultValue: "Made up Street",
+                validators: [notNull()],
+              },
+              {
+                label: "Recipient Town",
+                id: "recipent-street-address-town",
+                defaultValue: "Made up Town",
+                validators: [],
+              },
+              {
+                label: "Recipient City",
+                id: "recipent-street-address-city",
+                defaultValue: "Made up City",
+                validators: [],
+              },
+              {
+                label: "Recipient Postcode",
+                id: "recipent-street-address-postcode",
+                defaultValue: "M27 1XR",
+                validators: [notNull()],
+              },
+              {
+                label: "Encounter Start Datetime",
+                id: "nhs111-encounter-date-time-start",
+                defaultValue: "202201061200",
+                validators: [notNull()],
+              },
+              {
+                label: "Encounter End Datetime",
+                id: "nhs111-encounter-date-time-end",
+                defaultValue: "2022010613",
+                validators: [notNull()],
+              },
+              {
+                label: "Disposition Code",
+                id: "disposition-code",
+                defaultValue: "Dx001",
+                validators: [notNull(), minLength(4)],
+              },
+              {
+                label: "Disposition Text",
+                id: "disposition-text",
+                defaultValue: "Passed onto clinical hub",
+                validators: [notNull()],
+              },
+            ],
+          },
+        },
       ],
     },
   ],
