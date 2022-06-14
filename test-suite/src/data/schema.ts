6412365f2b1a928bfbb6ee3016b60ca5a95cb1e8
BenHession <bshession@gmail.com>
2022-06-14 11:25:27 +0100

    Additional formatting after rebase.

--- test-suite/src/data/schema.ts
+++ test-suite/src/data/schema.ts
@@ -557,138 +557,138 @@ const schema: Schema = {
                 defaultValue: "application/xml",
                 validators: [notNull(), minLength(7)]
               }
-              ],
-              [RequestHeaderProps.Body]: [
-                {
-                  label: "NHS Number",
-                  id: "nhs-number",
-                  defaultValue: "1234567891",
-                  validators: [notNull(), isNumeric(), isLength(10)]
-                },
-                {
-                  label: "Recipient ODS Code",
-                  id: "ods-code",
-                  defaultValue: "E88122002",
-                  validators: [notNull(), isAlphanumeric()]
-                },
-                {
-                  label: "Recipient DOS Code",
-                  id: "dos-code",
-                  defaultValue: "159744",
-                  validators: [isAlphanumeric()]
-                },
-                {
-                  label: "Recipient Name",
-                  id: "recipient-name",
-                  defaultValue: "Bramley Road Surgery",
-                  validators: [notNull(), isAlpha()]
-                },
-                {
-                  label: "Recipient Address Line 1",
-                  id: "recipient-street-address-line-1",
-                  defaultValue: "Bramley Road Surgery",
-                  validators: [notNull(), isAlphanumeric()]
-                },
-                {
-                  label: "Recipient Address Line 2",
-                  id: "recipient-street-address-line-2",
-                  defaultValue: "2 Bramley Road",
-                  validators: [notNull(), isAlphanumeric()]
-                },
-                {
-                  label: "Recipient Town",
-                  id: "recipient-street-address-town",
-                  defaultValue: "Ealing",
-                  validators: [isAlpha()]
-                },
-                {
-                  label: "Recipient City",
-                  id: "recipient-street-address-city",
-                  defaultValue: "London",
-                  validators: [isAlpha()]
-                },
-                {
-                  label: "Recipient Postcode",
-                  id: "recipient-street-address-postcode",
-                  defaultValue: "W5 4SS",
-                  validators: [isAlphanumeric()]
-                },
-                {
-                  label: "Backup Recipient ODS Code",
-                  id: "ods-code-2",
-                  defaultValue: "E88122",
-                  validators: [notNull(), isAlphanumeric()]
-                },
-                {
-                  label: "Backup Recipient DOS Code",
-                  id: "dos-code-2",
-                  defaultValue: "161145",
-                  validators: [isAlphanumeric()]
-                },
-                {
-                  label: "Backup Recipient Name",
-                  id: "recipient-name-2",
-                  defaultValue: "Florence Road Surgery",
-                  validators: [notNull(), isAlpha()]
-                },
-                {
-                  label: "Backup Recipient Address Line 1",
-                  id: "recipient-street-address-line-1-2",
-                  defaultValue: "Florence Road Surgery",
-                  validators: [notNull(), isAlphanumeric()]
-                },
-                {
-                  label: "Backup Recipient Address Line 2",
-                  id: "recipient-street-address-line-2-2",
-                  defaultValue: "26 Florence Road",
-                  validators: [notNull(), isAlphanumeric()]
-                },
-                {
-                  label: "Backup Recipient Town",
-                  id: "recipient-street-address-town-2",
-                  defaultValue: "Ealing",
-                  validators: [isAlpha()]
-                },
-                {
-                  label: "Backup Recipient City",
-                  id: "recipient-street-address-city-2",
-                  defaultValue: "London",
-                  validators: [isAlpha()]
-                },
-                {
-                  label: "Backup Recipient Postcode",
-                  id: "recipient-street-address-postcode-2",
-                  defaultValue: "W5 3TX",
-                  validators: [isAlphanumeric()]
-                },
-                {
-                  label: "Encounter Start Datetime",
-                  id: "nhs111-encounter-date-time-start",
-                  defaultValue: "202206101312",
-                  validators: [notNull(), isNumeric()],
-                  placeholder: datePlaceholder
-                },
-                {
-                  label: "Encounter End Datetime",
-                  id: "nhs111-encounter-date-time-end",
-                  validators: [notNull(), isNumeric()],
-                  defaultValue: "202206101430",
-                  placeholder: datePlaceholder
-                },
-                {
-                  label: "Disposition Code",
-                  id: "disposition-code",
-                  defaultValue: "12345",
-                  validators: [notNull(), minLength(4)]
-                },
-                {
-                  label: "Disposition Text",
-                  id: "disposition-text",
-                  defaultValue: "TEST TEXT",
-                  validators: [notNull()]
-                }
-              ]
-            }
+            ],
+            [RequestHeaderProps.Body]: [
+              {
+                label: "NHS Number",
+                id: "nhs-number",
+                defaultValue: "1234567891",
+                validators: [notNull(), isNumeric(), isLength(10)]
+              },
+              {
+                label: "Recipient ODS Code",
+                id: "ods-code",
+                defaultValue: "E88122002",
+                validators: [notNull(), isAlphanumeric()]
+              },
+              {
+                label: "Recipient DOS Code",
+                id: "dos-code",
+                defaultValue: "159744",
+                validators: [isAlphanumeric()]
+              },
+              {
+                label: "Recipient Name",
+                id: "recipient-name",
+                defaultValue: "Bramley Road Surgery",
+                validators: [notNull(), isAlpha()]
+              },
+              {
+                label: "Recipient Address Line 1",
+                id: "recipient-street-address-line-1",
+                defaultValue: "Bramley Road Surgery",
+                validators: [notNull(), isAlphanumeric()]
+              },
+              {
+                label: "Recipient Address Line 2",
+                id: "recipient-street-address-line-2",
+                defaultValue: "2 Bramley Road",
+                validators: [notNull(), isAlphanumeric()]
+              },
+              {
+                label: "Recipient Town",
+                id: "recipient-street-address-town",
+                defaultValue: "Ealing",
+                validators: [isAlpha()]
+              },
+              {
+                label: "Recipient City",
+                id: "recipient-street-address-city",
+                defaultValue: "London",
+                validators: [isAlpha()]
+              },
+              {
+                label: "Recipient Postcode",
+                id: "recipient-street-address-postcode",
+                defaultValue: "W5 4SS",
+                validators: [isAlphanumeric()]
+              },
+              {
+                label: "Backup Recipient ODS Code",
+                id: "ods-code-2",
+                defaultValue: "E88122",
+                validators: [notNull(), isAlphanumeric()]
+              },
+              {
+                label: "Backup Recipient DOS Code",
+                id: "dos-code-2",
+                defaultValue: "161145",
+                validators: [isAlphanumeric()]
+              },
+              {
+                label: "Backup Recipient Name",
+                id: "recipient-name-2",
+                defaultValue: "Florence Road Surgery",
+                validators: [notNull(), isAlpha()]
+              },
+              {
+                label: "Backup Recipient Address Line 1",
+                id: "recipient-street-address-line-1-2",
+                defaultValue: "Florence Road Surgery",
+                validators: [notNull(), isAlphanumeric()]
+              },
+              {
+                label: "Backup Recipient Address Line 2",
+                id: "recipient-street-address-line-2-2",
+                defaultValue: "26 Florence Road",
+                validators: [notNull(), isAlphanumeric()]
+              },
+              {
+                label: "Backup Recipient Town",
+                id: "recipient-street-address-town-2",
+                defaultValue: "Ealing",
+                validators: [isAlpha()]
+              },
+              {
+                label: "Backup Recipient City",
+                id: "recipient-street-address-city-2",
+                defaultValue: "London",
+                validators: [isAlpha()]
+              },
+              {
+                label: "Backup Recipient Postcode",
+                id: "recipient-street-address-postcode-2",
+                defaultValue: "W5 3TX",
+                validators: [isAlphanumeric()]
+              },
+              {
+                label: "Encounter Start Datetime",
+                id: "nhs111-encounter-date-time-start",
+                defaultValue: "202206101312",
+                validators: [notNull(), isNumeric()],
+                placeholder: datePlaceholder
+              },
+              {
+                label: "Encounter End Datetime",
+                id: "nhs111-encounter-date-time-end",
+                validators: [notNull(), isNumeric()],
+                defaultValue: "202206101430",
+                placeholder: datePlaceholder
+              },
+              {
+                label: "Disposition Code",
+                id: "disposition-code",
+                defaultValue: "12345",
+                validators: [notNull(), minLength(4)]
+              },
+              {
+                label: "Disposition Text",
+                id: "disposition-text",
+                defaultValue: "TEST TEXT",
+                validators: [notNull()]
+              }
+            ]
+          }
         }
       ]
     }
