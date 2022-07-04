f989f78849338b32ffed3ffeff812fc47a854863
roberttuppenanswerdigital <104197972+roberttuppenanswerdigital@users.noreply.github.com>
2022-07-04 13:45:28 +0100

    Remove Alpha validation from nullable fields (#254)
    * Remove Alpha validation from nullable fields
    
    * Update isAlpha and isAlphaNumeric to allow empty values where it doesnt have NotNull annotation
    
    * Fixing merge changes

--- test-suite/client/src/utils/validators.ts
+++ test-suite/client/src/utils/validators.ts
@@ -85,6 +85,10 @@ export const validateField = (fieldValidation: FormError, value: string) =>
       isError = typeof v.match === "number" && value.length < v.match;
     } else if (k === "hasLength" && value) {
       isError = typeof v.match === "number" && value.length === v.match;
+    } else if(k === "alphaMatch") {
+      isError = !(value === null || value === "") && (v.match instanceof RegExp && !new RegExp(v.match).test(value));
+    } else if(k === "alphaNumericMatch") {
+      isError = !(value === null || value === "") && (v.match instanceof RegExp && !new RegExp(v.match).test(value));
     } else if (value) {
       isError = v.match instanceof RegExp && !new RegExp(v.match).test(value);
     }
