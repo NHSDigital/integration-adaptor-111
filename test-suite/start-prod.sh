300c624bdca21488b6e4e33df90bab82fe0f9937
Rio Knightley <rio.knightley@madetech.com>
2022-07-13 15:00:37 +0100

    Deploy script spelling

--- test-suite/start-prod.sh
+++ test-suite/start-prod.sh
@@ -1,37 +1,70 @@
 set -e
 
-LIGHT_GREEN='\033[1;32m'
-RED='\033[0;31m'
-NC='\033[0m'
-
 app_install()
 {
-      echo "\n${LIGHT_GREEN}Installing client dependencies${NC}\n"
+      echo "
+      
+      Installing client dependencies
+      
+      "
       npm --prefix client/ install
 
-      echo "\n${LIGHT_GREEN}Installing server dependencies${NC}\n"
+      echo "
+      
+      Installing server dependencies
+      
+      "
       npm --prefix server/ install
 
-      echo "\n${LIGHT_GREEN}Building client for production...${NC}\n"
+      echo "
+      
+      Building client for production...
+      
+      "
       npm --prefix client/ run build
 
-      echo "\n${LIGHT_GREEN}Building server for production...${NC}\n"
+      echo "
+      
+      Building server for production...
+      
+      "
       npm --prefix server/ run build
 
-      echo "\n${LIGHT_GREEN}Outputing client html into html folder...${NC}\n"
+      echo "
+      
+      Outputing client html into html folder...
+      
+      "
       mkdir -p html
       cp client/dist/* html/
       
-      echo "\n${LIGHT_GREEN}Starting the test suite server...${NC}\n"
+      echo "
+      
+      Starting the test suite server...
+      
+      "
       npx pm2 delete TestSuiteServer  2> /dev/null || true && npx pm2 start  server/dist/index.js --name TestSuiteServer
 
-      echo "\n${LIGHT_GREEN}Done! Server running on port 7000. \n Remember to relocate html files to your web server html folder. \n \n Use 'npx pm2 ps' to see your server instance\n Use 'npx pm2 delete/stop/start TestSuiteServer' for running the instance${NC}\n"
+      echo "
+      
+      Done! Server running on port 7000.
+      
+      Remember to relocate html files to your web server html folder.
+      
+      Use 'npx pm2 ps' to see your server instance
+      Use 'npx pm2 delete/stop/start TestSuiteServer' for running the instance
+      
+      "
 }
 
 if ! command -v node &> /dev/null
   then
-    echo "\n${RED}Couldn't find npm or Node.
-    please install at https://nodejs.org/en/${NC}\n"
+    echo "
+    
+    Couldn't find npm or Node.
+    please install at https://nodejs.org/en/
+    
+    "
     exit 1  
   else
    app_install
