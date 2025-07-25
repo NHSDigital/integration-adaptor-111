303c81e3951c57539f4e8b375081ca129adb4204
MartinWheelerMT <martin.wheeler@madetech.com>
2025-07-25 14:14:30 +0100

    NIAD-3369: Update XmlBeans to version `4.0.0`
    * Update Schema JAR file to one having been built using XmlBeans `4.0.0`.
    * Update XML validator to use the base exception type of `XmlException` rather than `XmlValidationExceptions` as this is what is now used in `4.0.0`.
    * Update build.gradle to use the latest version of the `iucds-schema`.
    * Fix minor language errors in the validator which are producing warnings now.

--- service/src/main/java/uk/nhs/adaptors/oneoneone/xml/XmlValidator.java
+++ service/src/main/java/uk/nhs/adaptors/oneoneone/xml/XmlValidator.java
@@ -1,26 +1,25 @@
 package uk.nhs.adaptors.oneoneone.xml;
 
-import static java.util.stream.Collectors.toList;
-
 import static org.apache.logging.log4j.util.Strings.join;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
+import org.apache.xmlbeans.XmlError;
 import org.apache.xmlbeans.XmlException;
 import org.apache.xmlbeans.XmlObject;
 import org.apache.xmlbeans.XmlOptions;
-import org.apache.xmlbeans.XmlValidationError;
 
 public final class XmlValidator {
     public static void validate(XmlObject xmlObject) throws XmlException {
-        List<XmlValidationError> errors = new ArrayList<>();
+        Collection<XmlError> errors = new ArrayList<>();
         XmlOptions xmlOptions = new XmlOptions();
         xmlOptions.setErrorListener(errors);
         xmlObject.validate(xmlOptions);
 
         if (!errors.isEmpty()) {
-            List<String> messages = errors.stream().map(it -> it.getMessage()).collect(toList());
+            List<String> messages = errors.stream().map(XmlError::getMessage).toList();
             throw new XmlException(join(messages, '\n'));
         }
     }
