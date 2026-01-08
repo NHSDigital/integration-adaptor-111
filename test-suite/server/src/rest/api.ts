48da0cdf0b6756080a82aef7339282611a98652f
MartinWheelerMT <martin.wheeler@madetech.com>
2026-01-08 15:36:10 +0000

    Replace obsolete `Request` package with `Axios` in `test-suite/server`
    * Replace all references to `request` and references to types created for request with equivalent `axios` packages.
    * Refactor `api.ts` to use axios, adjusting the handling of requests and responses accordingly.
    * Remove now obsolete types from `index.ts`.
    * Update `package.json` and auto-generate `package-lock.json` to reflect the package changes.

--- test-suite/server/src/rest/api.ts
+++ test-suite/server/src/rest/api.ts
@@ -1,28 +1,20 @@
-import { Request as Request, Response, Router } from "express";
-import request from "request";
+import { Request as ExpressRequest, Response, Router } from "express";
+import axios, { AxiosError } from "axios";
 import multiparty from "multiparty";
 import fs from "fs";
+import https from "https";
 import {
-  ReqBody,
-  ReqError,
-  ReqOptions,
-  ReqResponse,
   MultiPartForm,
   FileTuple,
 } from "../types";
 import { AdaptorRequest, AdaptorResponse } from "@server/types";
 
-import { AgentOptions } from "https";
-
 const api = (router: Router) => {
-  router.post("/report", async (req: Request, res: Response) => {
+  router.post("/report", async (req: ExpressRequest, res: Response) => {
     const formPromise: Promise<MultiPartForm> = new Promise((resolve) => {
       const multipartForm = new multiparty.Form();
       multipartForm.parse(req, (_err, fields, files) => {
-        resolve({
-          fields,
-          files,
-        });
+        resolve({ fields, files });
       });
     });
     const { fields, files } = await formPromise;
@@ -35,12 +27,13 @@ const api = (router: Router) => {
     );
 
     const { url } = form.requestHeaderFields;
-    const requiresCert = url.includes("https");
+    const isHttps = url.startsWith("https");
     const caCert = getAgentKey(ca);
     const caKey = getAgentKey(key);
     const p12Key = getAgentKey(p12);
-    let agentOptions: AgentOptions = {};
-    if (requiresCert) {
+    let httpsAgent: https.Agent | undefined;
+
+    if (isHttps) {
       if (!caCert || !caKey || !p12Key) {
         const rejectResponse: AdaptorResponse = {
           apiStatus: 400,
@@ -49,44 +42,49 @@ const api = (router: Router) => {
         };
         res.json(rejectResponse).end();
         return;
-      } else {
-        agentOptions = {
-          ca: getAgentKey(ca),
-          key: getAgentKey(key),
-          // Or use `pfx` property replacing `cert` and `key` when using private key, certificate and CA certs in PFX or PKCS12 format:
-          pfx: getAgentKey(p12),
-          passphrase: password ? password[0] : undefined,
-          rejectUnauthorized: true,
-        };
       }
+
+      const agentOptions: https.AgentOptions = {
+        ca: caCert,
+        key: caKey,
+        pfx: p12Key,
+        passphrase: password ? password[0] : undefined,
+        rejectUnauthorized: true,
+      };
+
+      httpsAgent = new https.Agent(agentOptions);
     }
-    const options: ReqOptions = {
-      url,
-      headers: {
-        "Content-Type": form.requestHeaderFields["content-type"],
-        "Content-Length": xmlPayload.length,
-      },
-      agentOptions,
-      body: xmlPayload,
-    };
-    request.post(
-      options,
-      (error: ReqError, response: ReqResponse, body: ReqBody) => {
-        let adaptorResponse: AdaptorResponse = {
-          apiStatus: 200,
-          adaptorStatus: error?.errno ?? 400,
-          message: error?.code ?? "UNKNOWN_ERROR",
-        };
-        if (response && body) {
-          adaptorResponse = {
-            apiStatus: 200,
-            adaptorStatus: response.statusCode,
-            message: body,
-          };
-        }
-        res.json(adaptorResponse).end();
-      }
-    );
+
+    try {
+      const response = await axios.post<string>(url, xmlPayload, {
+        headers: {
+          "Content-Type": form.requestHeaderFields["content-type"],
+          "Content-Length": Buffer.byteLength(xmlPayload, "utf8"),
+        },
+        httpsAgent,
+        responseType: "text",
+        transformResponse: [(data) => data],
+        validateStatus: () => true,
+      });
+
+      const adaptorResponse: AdaptorResponse = {
+        apiStatus: 200,
+        adaptorStatus: response.status,
+        message: response.data ?? "",
+      };
+
+      res.json(adaptorResponse).end();
+    } catch (e) {
+      const err = e as AxiosError;
+
+      const adaptorResponse: AdaptorResponse = {
+        apiStatus: 200,
+        adaptorStatus: err.response?.status ?? (err as any)?.errno ?? 400,
+        message: err.code ?? err.message ?? "UNKNOWN_ERROR",
+      };
+
+      res.json(adaptorResponse).end();
+    }
   });
   return router;
 };
--- test-suite/server/src/types/index.ts
+++ test-suite/server/src/types/index.ts
@@ -1,17 +1,4 @@
 import { File } from "multiparty";
-import { Options, Response, Request } from "request";
-
-export type ReqBody = Response["body"] | undefined;
-export type ReqOptions = Options;
-export type ReqResponse = Response | undefined;
-export type ReqRequest = Request;
-export type ReqError = {
-  errno?: number;
-  code?: string;
-  syscall?: string;
-  address?: string;
-  port: number;
-} | null;
 
 export type FileTuple = [File];
 type StringTuple = [string];
